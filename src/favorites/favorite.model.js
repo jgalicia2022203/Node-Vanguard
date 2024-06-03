2a53719b0a5a3cc60feaee052cfd018921484a56
jgalicia-2022203 <jgalicia-2022203@kinal.edu.gt>
2024-06-03 15:45:54 -0600

    Favorite Accounts Model

---
+++ src/favorites/favorite.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const FavoriteSchema = new mongoose.Schema({
+  account_no: {
+    type: String,
+    required: [true, "account number is required"],
+    unique: true,
+  },
+  favorite_account_no: {
+    type: String,
+    required: [true, "favorite account number is required"],
+    unique: true,
+  },
+  alias: {
+    type: String,
+    required: [true, "alias is required"],
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+FavoriteSchema.methods.toJSON = function () {
+  const favorite = this.toObject();
+  return favorite;
+};
+
+export default mongoose.model("Favorite", FavoriteSchema);
